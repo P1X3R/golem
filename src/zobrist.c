7ae0fe4b09db7549a4e7f6eecb8ae3f3ef48cd89
P1X3R <p1x3r@proton.me>
2025-12-06 17:22:54 -0500

    feat: add zobrist tables

---
+++ src/zobrist.c
@@ -0,0 +1,30 @@
+#include "zobrist.h"
+
+#include <stdint.h>
+
+#include "defs.h"
+#include "rand.h"
+
+uint64_t ZOBRIST_PIECES[NR_OF_PIECE_TYPES][NR_OF_SQUARES];
+uint64_t ZOBRIST_COLOR;
+uint64_t ZOBRIST_CASTLING_RIGHTS[16];  // 2^4 = 16
+uint64_t ZOBRIST_EP_FILE[NR_OF_ROWS];
+
+void init_zobrist_tables() {
+  for (square_t sq = SQ_A1; sq <= SQ_H8; sq++) {
+    for (piece_t piece = PT_PAWN; piece <= PT_KING; piece++) {
+      ZOBRIST_PIECES[piece][sq] = random_u64();
+    }
+  }
+
+  ZOBRIST_COLOR = random_u64();
+
+  for (uint8_t rights_combination = 0; rights_combination < 16;
+       rights_combination++) {
+    ZOBRIST_CASTLING_RIGHTS[rights_combination] = random_u64();
+  }
+
+  for (uint8_t file = 0; file < NR_OF_ROWS; file++) {
+    ZOBRIST_EP_FILE[file] = random_u64();
+  }
+}
---
+++ src/zobrist.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "defs.h"
+
+extern uint64_t ZOBRIST_PIECES[NR_OF_PIECE_TYPES][NR_OF_SQUARES];
+extern uint64_t ZOBRIST_COLOR;
+extern uint64_t ZOBRIST_CASTLING_RIGHTS[16];  // 2^4 = 16
+extern uint64_t ZOBRIST_EP_FILE[NR_OF_ROWS];
+
+void init_zobrist_tables();
