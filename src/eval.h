8bd7ad157a3f5af572bc242aae260006f785326b
P1X3R <p1x3r@proton.me>
2025-12-26 11:23:50 -0500

    style: move evaluation function definition to header file

--- src/eval.h
+++ src/eval.h
@@ -3,5 +3,17 @@
 #include <stdint.h>
 
 #include "board.h"
+#include "psqt.h"
 
-int16_t static_eval(const board_t* board);
+static int16_t static_eval(const board_t* board) {
+  const int32_t mg_score =
+      board->mg_score[CLR_WHITE] - board->mg_score[CLR_BLACK];
+  const int32_t eg_score =
+      board->eg_score[CLR_WHITE] - board->eg_score[CLR_BLACK];
+  const uint8_t mg_phase =
+      (board->phase > TOTAL_PHASE) ? TOTAL_PHASE : board->phase;
+  const uint8_t eg_phase = TOTAL_PHASE - mg_phase;
+
+  return (int16_t)(((mg_score * mg_phase) + (eg_score * eg_phase)) /
+                   TOTAL_PHASE);
+}
